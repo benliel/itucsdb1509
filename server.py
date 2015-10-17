284a9f995600d47a4eef0069132b70bf0e1f7e33
ilkengin <engini@itu.edu.tr>
2015-10-17 22:29:16 +0300

    Try

--- server.py
+++ server.py
@@ -1,18 +1,22 @@
 import datetime
-import os
-
 from flask import Flask
 from flask import render_template
 
-#test
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+
+@app.route('/') 
+def home_page():
     now = datetime.datetime.now()
     return render_template('home.html', current_time=now.ctime())
+    
+    
+@app.route('/movies') 
+def movies_page():
+    return render_template('movies.html')
 
 
-if __name__ == '__main__':
-    PORT = int(os.getenv('VCAP_APP_PORT', '5000'))
-    app.run(host='0.0.0.0', port=int(PORT))
+if __name__ == '__main__': 
+    app.run(host='0.0.0.0', port=5000)
+

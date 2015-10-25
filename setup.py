d085044c765a7e0a41d4ba41dd002f3fb3aa17ad
ilkengin <engini@itu.edu.tr>
2015-10-25 13:45:16 +0200

    Some arrangements

---
+++ setup.py
@@ -0,0 +1,27 @@
+__author__ = "ilkengin"
+__date__ = "$Oct 25, 2015 1:39:07 PM$"
+
+from setuptools import setup, find_packages
+
+setup (
+       name='itucsdb1509',
+       version='0.1',
+       packages=find_packages(),
+
+       # Declare your packages' dependencies here, for eg:
+       install_requires=['foo>=3'],
+
+       # Fill in these to make your Egg ready for upload to
+       # PyPI
+       author='ilkengin',
+       author_email='',
+
+       summary='Just another Python package for the cheese shop',
+       url='',
+       license='',
+       long_description='Long description of the package',
+
+       # could also include long_description, download_url, classifiers, etc.
+
+  
+       )

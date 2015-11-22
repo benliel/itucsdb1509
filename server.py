56834c195274896d1fc87f236e08b93e189cbee9
semakarakas <semakarakas@semakarakas-Lenovo-G570>
2015-11-22 18:28:16 +0200

    Solving conflicts

--- server.py
+++ server.py
@@ -13,6 +13,7 @@ from store import Store
 from fixture import *
 from sponsors import *
 from curlers import *
+from clubs import *
 
 app = Flask(__name__)
 
@@ -39,7 +40,8 @@ def initialize_database():
     init_fixture_db(cursor)
     init_sponsors_db(cursor)
     init_curlers_db(cursor)
-
+    init_clubs_db(cursor)
+    
     #championships table creation
     query = """DROP TABLE IF EXISTS CHAMPIONSHIP"""
     cursor.execute(query)
@@ -153,19 +155,25 @@ def clubs_page():
         cursor.execute(query)
 
         return render_template('clubs.html', clubs = cursor, current_time=now.ctime())
-    else:
-        name = request.form['name']
-        place = request.form['place']
-        year = request.form['year']
-        chair = request.form['chair']
-        number_of_members = request.form['number_of_members']
-        reward_number = request.form['reward_number']
-        query = """INSERT INTO CLUBS (NAME, PLACE, YEAR, CHAIR, NUMBER_OF_MEMBERS,REWARDNUMBER)
-        VALUES ('"""+name+"', '"+place+"', '"+year+"' , '"+chair+"', '"+number_of_members+"', '"+reward_number+"')"
-        cursor.execute(query)
+    elif "add" in request.form:
+        club = Clubs(request.form['name'],
+                     request.form['place'],
+                     request.form['year'],
+                     request.form['chair'],
+                     request.form['number_of_members'],
+                     request.form['rewardnumber'])
+
+        add_club(cursor, request, club)
+
         connection.commit()
         return redirect(url_for('clubs_page'))
+    elif "delete" in request.form:
+        for line in request.form:
+            if "checkbox" in line:
+                delete_club(cursor, int(line[9:]))
+                connection.commit()
 
+        return redirect(url_for('clubs_page'))
 
 ##Sponsorships arrangements by Muhammed Aziz Ulak
 @app.route('/sponsors', methods=['GET', 'POST'])

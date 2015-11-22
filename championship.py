33aec582f70a3397495b5ace6743b854a5d48285
huseyinyusuf <giousouf@itu.edu.tr>
2015-11-22 20:39:46 +0200

    Delete method added to championship table

--- championship.py
+++ championship.py
@@ -1,4 +1,4 @@
-class Championship:
+class Championships:
     def __init__(self, name, place, date, type, teamNo,reward):
         self.name = name
         self.place = place
@@ -7,22 +7,23 @@ class Championship:
         self.teamNo = teamNo
         self.reward = reward
 
-def init_championship_db(cursor):
+def init_championships_db(cursor):
     query = """DROP TABLE IF EXISTS CHAMPIONSHIP"""
     cursor.execute(query)
     query = """CREATE TABLE CHAMPIONSHIP (
-ID SERIAL,
-NAME VARCHAR(80) NOT NULL,
-PLACE VARCHAR(80) NOT NULL,
-DATE DATE NOT NULL,
-TYPE VARCHAR(80) NOT NULL,
-NUMBER_OF_TEAMS INTEGER NOT NULL,
-REWARD VARCHAR(80),
-PRIMARY KEY(ID)
-)"""
+    ID SERIAL,
+    NAME VARCHAR(80) NOT NULL,
+    PLACE VARCHAR(80) NOT NULL,
+    DATE DATE NOT NULL,
+    TYPE VARCHAR(80) NOT NULL,
+    NUMBER_OF_TEAMS INTEGER NOT NULL,
+    REWARD VARCHAR(80),
+    PRIMARY KEY(ID)
+    )"""
     cursor.execute(query)
 
-def add_championship(cursor, request, championship):
+def add_championship(cursor, request, championship1):
+        print("Try")
         query = """INSERT INTO CHAMPIONSHIP
         (NAME, PLACE, DATE, TYPE, NUMBER_OF_TEAMS,REWARD) VALUES (
         %s,
@@ -32,8 +33,10 @@ def add_championship(cursor, request, championship):
         %s,
         %s
         )"""
-        cursor.execute(query, (Championship.name, Championship.place, Championship.date,
-                                Championship.type, Championship.teamNo,Championship.reward))
+        cursor.execute(query, (championship1.name, championship1.place, championship1.date,
+                                championship1.type, championship1.teamNo,championship1.reward))
+
+
 
 def delete_championship(cursor, id):
         query="""DELETE FROM CHAMPIONSHIP WHERE ID = %s"""
--- server.py
+++ server.py
@@ -12,7 +12,7 @@ from flask.helpers import url_for
 from store import Store
 from fixture import *
 from sponsors import *
-from curlers import *
+from championship import *
 
 app = Flask(__name__)
 
@@ -38,7 +38,7 @@ def initialize_database():
 
     init_fixture_db(cursor)
     init_sponsors_db(cursor)
-    init_curlers_db(cursor)
+    init_championships_db(cursor)
 
     #championships table creation
     query = """DROP TABLE IF EXISTS CHAMPIONSHIP"""
@@ -76,33 +76,33 @@ def initialize_database():
 
 @app.route('/championships', methods=['GET', 'POST'])
 def championships_page():
-    now = datetime.datetime.now()
     connection = dbapi2.connect(app.config['dsn'])
     cursor = connection.cursor()
-
     if request.method == 'GET':
-        query = """SELECT * FROM CHAMPIONSHIP"""
+        now = datetime.datetime.now()
+        query = "SELECT * FROM CHAMPIONSHIP"
         cursor.execute(query)
 
-        return render_template('championships.html', championships = cursor, current_time = now.ctime())
-    else:
-        name =request.form['name']
-        place =request.form['place']
-        date =request.form['date']
-        type =request.form['type']
-        teamNumber =request.form['number_of_teams']
-        reward =request.form['reward']
+        return render_template('championships.html', championship = cursor, current_time = now.ctime())
+    elif "add" in request.form:
+        championship1 = Championships(request.form['name'],
+                     request.form['place'],
+                     request.form['date'],
+                     request.form['type'],
+                     request.form['number_of_teams'],
+                     request.form['reward'])
 
-        query = """INSERT INTO CHAMPIONSHIP(NAME, PLACE, DATE, TYPE, NUMBER_OF_TEAMS, REWARD) VALUES (
-        '"""+ name +"""',
-        '"""+ place +"""',
-        to_date('"""+date+"""', 'DD.MM.YYYY'),
-        '"""+ type +"""',
-        """+ teamNumber +""",
-        '"""+ reward+"')"
-        cursor.execute(query)
+        add_championship(cursor, request, championship1)
 
         connection.commit()
+        return redirect(url_for('championships_page'))
+
+    elif "delete" in request.form:
+        for line in request.form:
+            if "checkbox" in line:
+                delete_championship(cursor, int(line[9:]))
+                connection.commit()
+
         return redirect(url_for('championships_page'))
 
 @app.route('/fixture', methods=['GET', 'POST'])
@@ -127,18 +127,18 @@ def curlers_page():
                      request.form['team'],
                      request.form['country'])
 
-        add_curler(cursor, request, curler)
+        add_curler(cursor, request, )
 
         connection.commit()
-        return redirect(url_for('curlers_page'))
+        return redirect(url_for('s_page'))
 
     elif "delete" in request.form:
         for line in request.form:
             if "checkbox" in line:
-                delete_curler(cursor, int(line[9:]))
+                delete_(cursor, int(line[9:]))
                 connection.commit()
 
-        return redirect(url_for('curlers_page'))
+        return redirect(url_for('s_page'))
 
 
 ##Sema's Part - Curling Clubs

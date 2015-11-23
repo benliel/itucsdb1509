c406e8e6362234176cb83dc8599b6b80021bdc9d
ilkengin <engini@itu.edu.tr>
2015-11-24 01:36:15 +0200

    Added search method to curlers Added referance to club of curlers

--- curlers.py
+++ curlers.py
@@ -1,42 +1,42 @@
 class Curler:
-    def __init__(self, name, surname, birthdate, team, country):
-        self.name = name
-        self.surname = surname
+    def __init__(self, name, surname, birthdate, teamid, country):
+        self.curler_name = name
+        self.curler_surname = surname
         self.birthdate = birthdate
-        self.team = team
+        self.teamid = teamid
         self.country = country
 
 def init_curlers_db(cursor):
     query = """DROP TABLE IF EXISTS CURLERS"""
     cursor.execute(query)
     query = """CREATE TABLE CURLERS (
-            ID SERIAL,
-            NAME varchar(80) NOT NULL,
-            SURNAME varchar(80) NOT NULL,
+            CURLERID SERIAL,
+            CURLER_NAME varchar(80) NOT NULL,
+            CURLER_SURNAME varchar(80) NOT NULL,
             BIRTH_DATE varchar(20),
-            TEAM varchar(20),
+            TEAMID integer REFERENCES CLUBS(ID) ON UPDATE CASCADE ON DELETE CASCADE,
             COUNTRY varchar(80),
-            PRIMARY KEY (ID)
+            PRIMARY KEY (CURLERID)
             )"""
     cursor.execute(query)
 
 def add_curler(cursor, request, curler):
     query = """INSERT INTO CURLERS
-    (NAME, SURNAME, BIRTH_DATE, TEAM, COUNTRY) VALUES (
+    (CURLER_NAME, CURLER_SURNAME, BIRTH_DATE, TEAMID, COUNTRY) VALUES (
     %s,
     %s,
     %s,
     %s,
     %s
     )"""
-    cursor.execute(query, (curler.name, curler.surname, curler.birthdate,
-                           curler.team, curler.country))
+    cursor.execute(query, (curler.curler_name, curler.curler_surname, curler.birthdate,
+                           curler.teamid, curler.country))
 
 def delete_curler(cursor, id):
-    query="""DELETE FROM CURLERS WHERE ID = %s"""
+    query="""DELETE FROM CURLERS WHERE CURLERID = %s"""
     cursor.execute(query, (int(id),))
 
 def update_curler(cursor,curler,id):
-    query="""UPDATE CURLERS SET NAME=%s,SURNAME=%s,BIRTH_DATE=%s,TEAM=%s,COUNTRY=%s WHERE(ID=%s)"""
-    cursor.execute(query, (curler.name,curler.surname,curler.birthdate, 
-                   curler.team,curler.country,id))
+    query="""UPDATE CURLERS SET CURLER_NAME=%s,CURLER_SURNAME=%s,BIRTH_DATE=%s,TEAMID=%s,COUNTRY=%s WHERE(CURLERID=%s)"""
+    cursor.execute(query, (curler.curler_name,curler.curler_surname,curler.birthdate, 
+                   curler.teamid,curler.country,id))

90bc2cae9a8c2c017c8ef53d6f388f5153fbd24d
mazizulak <mazula95@gmail.com>
2015-11-23 22:35:32 +0200

    Update operations has been set

--- sponsors.py
+++ sponsors.py
@@ -29,7 +29,7 @@ def init_sponsors_db(cursor):
     query = """CREATE TABLE SPONSORS (
     ID SERIAL,
     NAME VARCHAR(80) NOT NULL,
-    SUPPORTEDTEAM VARCHAR(80) NOT NULL,
+    SUPPORTEDTEAM INT NOT NULL REFERENCES CLUBS(ID),
     BUDGET INTEGER NOT NULL,
     PRIMARY KEY (ID)
     )"""
@@ -48,38 +48,18 @@ def delete_sponsor(cursor, id):
         query="""DELETE FROM SPONSORS WHERE ID = %s"""
         cursor.execute(query, (int(id),))
 
-def update_sponsor_name(curser, id, name, supportedteam, budget):
-        query="""UPDATE SPONSORS
-        SET NAME = '%s'
-        WHERE ID = %s;"""
-        cursor.execute(query, (name,int(id)))
-
-def update_sponsor_supportedteam(curser, id,supportedteam):
-        query="""UPDATE SPONSORS
-        SET SUPPORTEDTEAM = '%s'
-        WHERE ID = %s;"""
-        cursor.execute(query, (supportedteam,int(id)))
-
-def update_sponsor_budget(curser, id,budget):
-        query="""UPDATE SPONSORS
-        SET BUDGET = '%s'
-        WHERE ID = %s;"""
-        cursor.execute(query, (budget,int(id)))
-
-
 def search_sponsor(curser,name):
         query="""SELECT * FROM SPONSORS WHERE NAME = '%s%';"""
         curser.execute(query,name)
-def get_sponsors_page(app):
-    connection = dbapi2.connect(app.config['dsn'])
-    cursor = connection.cursor()
 
+def get_sponsors_page(app):
     if request.method == 'GET':
         now = datetime.datetime.now()
-        query = "SELECT * FROM SPONSORS"
-        cursor.execute(query)
+        sponsors = get_all_sponsors(app)
+        clubs = get_club_names(app)
 
-        return render_template('sponsors.html', sponsors = cursor, current_time=now.ctime())
+        return render_template('sponsors.html', sponsors = sponsors,
+                               clubs=clubs, current_time=now.ctime())
     elif "add" in request.form:
         sponsor = Sponsors(request.form['name'],
                      request.form['supportedteam'],
@@ -96,32 +76,110 @@ def get_sponsors_page(app):
                 connection.commit()
 
         return redirect(url_for('sponsors_page'))
-    elif "update" in request.form:
-        for line in request.form:
-            if "update" in line:
-                update_sponsor_name(curser, int(line[7:]), request.form['sponsorname'])
-                connection.commit()
-        return redirect(url_for('sponsors_page'))
 
 def get_sponsors_edit_page(app,sponsor_id):
     if request.method == 'GET':
-        now = datetime.datetime.now();
-        try:
-            connection = dbapi2.connect(app.config['dsn'])
-            try:
-                cursor = connection.cursor()
-                cursor.execute("""
-                SELECT * FROM SPONSORS WHERE (ID=%s)
-                """,sponsor_id)
-                sponsor = cursor.fetchone()
-            except:
-                cursor.rollback()
-            finally:
-                cursor.close()
-        except:
-            connection.rollback()
-        finally:
-            connection.close()
-        return render_template('sponsors_edit_page.html',current_time=now.ctime(),sponsor=sponsor)
+        now = datetime.datetime.now()
+        sponsor = get_sponsor(app, sponsor_id)
+        clubs = get_club_names(app)
+        return render_template('sponsors_edit_page.html', current_time=now.ctime(), sponsor=sponsor, clubs=clubs)
+
     if request.method == 'POST':
-        return redirect(url_for('sponsors_page'));
+        sponsor = Sponsors(request.form['name'],
+                      request.form['supportedteam'],
+                      request.form['budget'])
+        update_sponsor(app, request.form['id'], sponsor)
+        return redirect(url_for('sponsors_page'))
+
+def get_club_names(app):
+    clubs=None
+    connection=dbapi2.connect(app.config['dsn'])
+    try:
+        cursor = connection.cursor()
+        try:
+            cursor.execute('SELECT ID,NAME FROM CLUBS')
+            clubs = cursor.fetchall()
+        except dbapi2.Error as e:
+            print(e.pgerror)
+            cursor.rollback()
+        finally:
+            cursor.close()
+    except dbapi2.Error as e:
+        print(e.pgerror)
+        connection.rollback()
+    finally:
+        connection.close()
+        return clubs
+
+def get_sponsor(app, sponsor_id):
+    sponsor=None
+    connection = dbapi2.connect(app.config['dsn'])
+    try:
+        cursor = connection.cursor()
+        try:
+            cursor.execute('''
+            SELECT S.ID, T.NAME, S.NAME, S.BUDGET
+            FROM SPONSORS AS S,CLUBS AS T
+            WHERE (
+                S.ID=%s AND T.ID=S.SUPPORTEDTEAM
+                )
+            ''', sponsor_id);
+            sponsor = cursor.fetchone()
+        except dbapi2.Error as e:
+            print(e.pgerror)
+            cursor.rollback()
+        finally:
+            cursor.close()
+    except dbapi2.Error as e:
+        print(e.pgerror)
+        connection.rollback()
+    finally:
+        connection.close()
+        return sponsor
+
+def update_sponsor(app, id, sponsor):
+    connection = dbapi2.connect(app.config['dsn'])
+    try:
+        cursor = connection.cursor()
+        try:
+            cursor.execute("""
+            UPDATE SPONSORS
+            SET NAME = %s,
+            SUPPORTEDTEAM = %s,
+            BUDGET = %s
+            WHERE ID= %s
+            """, (sponsor.name, sponsor.supportedteam,
+                  sponsor.budget, id))
+            print("done")
+        except:
+            cursor.rollback()
+        finally:
+            cursor.close()
+    except:
+        connection.rollback()
+    finally:
+        connection.commit()
+        connection.close()
+
+def get_all_sponsors(app):
+    sponsors=None
+    connection = dbapi2.connect(app.config['dsn'])
+    try:
+        cursor=connection.cursor()
+        try:
+            cursor.execute('''
+            SELECT S.ID, T.NAME, S.NAME, S.BUDGET
+            FROM SPONSORS AS F, CLUBS AS T
+            WHERE(S.SUPPORTEDTEAM=T.ID)
+            ORDER BY 1
+            ''')
+            sponsors = cursor.fetchall()
+        except:
+            cursor.rollback()
+        finally:
+            cursor.close()
+    except:
+        connection.rollback()
+    finally:
+        connection.close()
+        return sponsors
